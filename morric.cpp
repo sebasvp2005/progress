544f7e116c39c1db62eb50f65f4da5a7c3e1b86f
sebasvp2005 <97813095+sebasvp2005@users.noreply.github.com>
2022-06-17 18:43:33 -0500

    Update morric.cpp

--- morric.cpp
+++ morric.cpp
@@ -7,11 +7,12 @@ using namespace std;
 typedef struct player
 {
     string name = "";
-    pair <bool, int>fichas[9];
+    pair <bool, pair <int, char>>fichas[9];
     
 }player;
 
 bool sameLocation(pair<int, char> pos);
+bool uselessLocation(pair <int, char> pos);
 
 HANDLE hconsole =GetStdHandle(STD_OUTPUT_HANDLE);
 
@@ -21,19 +22,20 @@ map <int, vector<char>> M={{1, {'B', 'C', 'E', 'F'}},
                             {4, {'D'}},
                             {5, {'A', 'B', 'F', 'G'}},
                             {6, {'A', 'C', 'E', 'G'}},
-                            {7, {'B', 'C', 'E', 'F'};
+                            {7, {'B', 'C', 'E', 'F'}}};
 
 
+
+player *PLAYER[2];
 
 int main()
 {
     //inicitialize variables and get data from players
-    player *PLAYER[2];
     PLAYER[0]= (player*) calloc(1, sizeof(player));
     PLAYER[1]= (player*) calloc(1, sizeof(player));
 
     string temp;
-    cout << PLAYER[0]->fichas->first << " " << PLAYER[0]->fichas->second << endl;
+    cout << PLAYER[0]->fichas->first << " " << PLAYER[0]->fichas->second.first << endl;
 
     cout << "Ingrese nombre de jugador 1 (blancas): \n";
     cin >> temp;
@@ -76,7 +78,7 @@ int main()
     //start phase 1
     for(int i=0; i<18; i++)
     {
-        cout << "Player " << (i%2)+1 << ": Introduzca posicion de ficha(ejem 1a): ";
+        cout << "Player " << (i%2)+1 << ": Introduzca posicion de ficha(ejem 1A): ";
 
         string in;
 
@@ -88,7 +90,7 @@ int main()
         while((int)in.size()!= 2 || !isdigit(in[0]) || !isalpha(in[1]))
         {
             cout << "Uso correcto (ejemplo): 1A" << endl;
-            cout << "Player " << (i%2)+1 << ": Introduzca posicion de ficha(ejem 1a): ";
+            cout << "Player " << (i%2)+1 << ": Introduzca posicion de ficha(ejem 1A): ";
             cin >> in;
         }
 
@@ -99,7 +101,10 @@ int main()
 
         pos.second= in[1];
 
-        if(!sameLocation(pos)) goodInput=true;
+        if(!uselessLocation(pos))
+        {
+            if(!sameLocation(pos)) goodInput=true;
+        }
         
         }while(!goodInput);
 
@@ -126,20 +131,31 @@ int main()
     free(PLAYER[1]);
 }
 
-bool uselessLocation(pair <int, char> pos)
+bool uselessLocation(pair <int, char> pos)  //check if the pos is a useless location
 {
     for (auto e: M)
     {
         if(e.first==pos.first)
         {
-            h
+            for(int i=0; i<(int) e.second.size(); i++)
+            {
+                if(e.second[i]==pos.second) return true;
+            }
         }
-
-        return false;
-        
     }
+    return false;
+
 }
-bool sameLocation(pair<int, char> pos){
+bool sameLocation(pair<int, char> pos){ //check if there is one more token with the same location
+
+    for(int i=0; i<2; i++)
+    {
+        for(int j=0; j<9; j++)
+        {
+            if(PLAYER[i]->fichas[j].first && PLAYER[i]->fichas[j].second.first==pos.first && PLAYER[i]->fichas[j].second.second == pos.second) return true;
+        }
+    }
 
     return false;
 }
+
