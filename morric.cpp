0012743002df7e0d6270568b8c3f243e075a15bf
sebasvp2005 <97813095+sebasvp2005@users.noreply.github.com>
2022-06-22 01:40:16 -0500

    Update morric.cpp

--- morric.cpp
+++ morric.cpp
@@ -1,5 +1,4 @@
-#include <iostream>
-#include <strings.h>
+#include <bits/stdc++.h>
 #include <conio.h>
 #include <windows.h>
 
@@ -12,24 +11,31 @@ using namespace std;
 typedef struct player
 {
     string name = "";
+    int tokenNum =9;
     pair <bool, pair <int, char>>fichas[9];
     
 }player;
 
+
 pair<int, int> Convertion (string in);
 bool sameLocation(pair<int, char> pos);
-bool uselessLocation(pair <int, char> pos);
+bool validLocation(pair <int, char> pos);
 void displayBoard ();
+bool foundNearPos (string in1, string in2);
 
 HANDLE hconsole =GetStdHandle(STD_OUTPUT_HANDLE);
 
-map <int, vector<char>> M={{1, {'B', 'C', 'E', 'F'}},
-                            {2, {'A', 'C', 'E', 'G'}},
-                            {3, {'A', 'B', 'F', 'G'}},
-                            {4, {'D'}},
-                            {5, {'A', 'B', 'F', 'G'}},
-                            {6, {'A', 'C', 'E', 'G'}},
-                            {7, {'B', 'C', 'E', 'F'}}};
+
+
+map <int, vector<char>> validPos={{1, {'A', 'D', 'G'}},
+                            {2, {'B', 'D', 'F'}},
+                            {3, {'C', 'D', 'E'}},
+                            {4, {'A','B', 'C', 'E', 'F', 'G'}},
+                            {5, {'C', 'D', 'E'}},
+                            {6, {'B', 'D', 'F'}},
+                            {7, {'A', 'D', 'G', }}};
+
+
 
 
 char board [][29]= {{"   1   2   3   4   5   6   7"},
@@ -48,7 +54,9 @@ char board [][29]= {{"   1   2   3   4   5   6   7"},
                     {"G  *-----------*-----------*"}
     };
 
+
 player *PLAYER[2];
+
 
 int main()
 {
@@ -57,7 +65,13 @@ int main()
     PLAYER[1]= (player*) calloc(1, sizeof(player));
 
     string temp;
-    cout << PLAYER[0]->fichas->first << " " << PLAYER[0]->fichas->second.first << endl;
+
+    cout<< "     __  ___                _                                   " << endl;   
+    cout<< "    /  |/  /___  __________(_)____   ____ _____ _____ ___  ___  " << endl;  
+    cout<< "   / /|_/ / __  / ___/ ___/ / ___/  / __ `/ __ `/ __ `__  / _   " << endl;  
+    cout<< "  / /  / / /_/ / /  / /  / (__  )  / /_/ / /_/ / / / / / /  __/ " << endl;  
+    cout<< " /_/  /_/ ____/_/  /_/  /_/____/    __, / __,_/_/ /_/ /_/ ___/  " << endl;  
+    cout<< "                                  /____/                        " << endl << endl; 
 
     cout << "Ingrese nombre de jugador 1 (blancas): \n";
     cin >> temp;
@@ -74,9 +88,10 @@ int main()
     for(int i=0; i<18; i++)
     {
         string in;
-        pair<int,int> pos;
+        pair<int,int> pos={1,1}; //random start value
 
         displayBoard(); 
+        cout << "Fase de colocar fichas" <<endl;
 
         do{
 
@@ -100,6 +115,62 @@ int main()
 
     }
 
+    //phase 2
+    int i=0;
+
+    while(PLAYER[0]->tokenNum <3 || PLAYER[1]->tokenNum <3 )
+    {
+
+        pair<int, int> pos1 ={1,1}; //random star value
+        pair<int, int> pos2;
+        string in1, in2;
+        bool back=false;
+     
+
+        displayBoard();
+
+        cout << "Fase mover fichas" <<endl;
+
+        do{
+
+
+        do{
+
+            if(pos1.first==0 && pos1.second==0) cout << "Uso correcto: 1A" << endl;
+
+            cout << "Player " << (i%2)+1 << ": Introduzca posicion de ficha(ejem 1A): ";
+            cin >> in1;
+            cin.ignore(123, '\n');
+
+            //check input's validity 
+            pos1 =Convertion(in1);
+            if(i%2==0) {if(board [pos1.second][pos1.first]!='w') pos1= {0,0};}
+            else{if(board [pos1.second][pos1.first]!='b') pos1= {0,0};}
+            
+        }
+        while(pos1.first==0 && pos1.second == 0);
+
+        do{
+        back=false;
+        cout << "Ingrese nueva posicion o ingrese ''Atras'' para selecionar otra ficha  : ";
+        cin >> in2;
+        cin.ignore(123, '\n');
+        if(strcmp(in2.c_str(), "Atras")==0) 
+            {back=true;
+            break;}
+        pos2= Convertion(in2);
+        if(board[pos2.second][pos2.first]!='*') pos2={0,0};
+        }while(pos2.first==0 && pos2.second == 0);
+
+        }while(back || foundNearPos(in1, in2));
+
+        
+
+
+
+        i++;
+    }
+
 
 
 
@@ -114,9 +185,9 @@ int main()
     free(PLAYER[1]);
 }
 
-/*bool uselessLocation(pair <int, char> pos)
+bool validLocation(pair <int, char> pos)
 {
-    for (auto e: M)
+    for (auto e: validPos)
     {
         if(e.first==pos.first)
         {
@@ -129,6 +200,7 @@ int main()
     return false;
 
 }
+/*
 bool sameLocation(pair<int, char> pos){
 
     for(int i=0; i<2; i++)
@@ -166,7 +238,7 @@ pair <int, int> Convertion (string in)
     else if(strcmp(in.c_str(), "5E")==0) return {19,9};
     else if(strcmp(in.c_str(), "6B")==0) return {23,3};
     else if(strcmp(in.c_str(), "6D")==0) return {23,7};
-    else if(strcmp(in.c_str(), "6F")==0) return {23,12};
+    else if(strcmp(in.c_str(), "6F")==0) return {23,11};
     else if(strcmp(in.c_str(), "7A")==0) return {27,1};
     else if(strcmp(in.c_str(), "7D")==0) return {27,7};
     else if(strcmp(in.c_str(), "7G")==0) return {27,13};
@@ -191,5 +263,60 @@ void displayBoard ()
     }
 
 }
+
+bool foundNearPos (string in1, string in2)
+{
+    for(int i=1; in1[1]-i>='A'; i++) //go up
+    {
+        pair <int, char> temp= {in1[0]-'0', in1[1]-i};
+        if(validLocation(temp))
+        {
+            if(temp== (pair<int, char>){in2[0]-'0', in2[1]}) return true;
+
+            break;
+        }
+    }
+
+    for(int i=1; in1[1]+i<='G'; i++) //go down
+    {
+        pair <int, char> temp= {in1[0]-'0', in1[1]+i};
+        if(validLocation(temp))
+        {
+            if(temp== (pair<int, char>){in2[0]-'0', in2[1]}) return true;
+
+            break;
+        }
+    }
+
+    for(int i=1; in1[0]-i>='1'; i++) //go left
+    {
+        pair <int, char> temp= {in1[0]-'0'-i, in1[1]};
+        if(validLocation(temp))
+        {
+            if(temp== (pair<int, char>){in2[0]-'0', in2[1]}) return true;
+
+            break;
+        }
+    }
+
+    for(int i=1; in1[0]+i<='7'; i++) //go right
+    {
+        pair <int, char> temp= {in1[0]-'0'+i, in1[1]};
+        if(validLocation(temp))
+        {
+            if(temp== (pair<int, char>){in2[0]-'0', in2[1]}) return true;
+
+            break;
+        }
+    }
+
+
+    return false;
+}
+
+
+
+
+
 
 
